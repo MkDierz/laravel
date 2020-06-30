4d4d9651b365f806340ae270c1b4b756dce21b43
MkDierz <msyibbran24@gmail.com>
2020-07-01 02:01:06 +0700

    all tugas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
+
+Route::get('/register', 'AuthController@index');
+
+Route::POST('/welcome', 'AuthController@proses');
+
